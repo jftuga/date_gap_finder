1ae332362f119970dc394827e4f9ab3f6cbaacff
John Taylor <JohnNoSpamTaylor@gmail.com>
2021-05-09 14:45:06 -0400

    updated / corrected tests

--- cmd/search_test.go
+++ cmd/search_test.go
@@ -39,21 +39,21 @@ func TestSearch1(t *testing.T) {
 	fname := "TestSearch1.csv"
 	data := "Date,Errors,Warnings\n2021-04-15 06:55:01,0,23\n2021-04-15 08:30:26,0,23\n2021-04-16 06:55:01,0,23\n2021-04-19 06:55:01,0,23"
 	CreateCSVFile(fname, data)
-	return
 
-	allRootOptions.Amount = 1442
+	allRootOptions.Amount = 1440
 	allRootOptions.Unit = "minutes"
+	allRootOptions.Padding = "10s"
 	allRootOptions.Column = 0
 	allRootOptions.CsvDelimiter = ","
 	allRootOptions.SkipWeekends = false
 	allRootOptions.HasNoHeader = false
 
 	missingDates, csvStyleDate := SearchOneFile(fname)
-	debug(missingDates, csvStyleDate)
+
 	iss := is.New(t)
 	iss.Equal(len(missingDates), 2)
-	iss.Equal(missingDates[0].ToTime().String(), "2021-04-17 06:59:01 +0000 UTC")
-	iss.Equal(missingDates[1].ToTime().String(), "2021-04-18 07:01:01 +0000 UTC")
+	iss.Equal(missingDates[0].ToTime().String(), "2021-04-17 06:55:01 +0000 UTC")
+	iss.Equal(missingDates[1].ToTime().String(), "2021-04-18 06:55:01 +0000 UTC")
 	iss.Equal(csvStyleDate, "2021-04-15 06:55:01")
 }
 
@@ -62,19 +62,19 @@ func TestSearch2(t *testing.T) {
 	fname := "TestSearch2.csv"
 	data := "Processed,Date\n5125,2021-04-12\n5197,2021-04-13\n5206,2021-04-14\n5222,2021-04-19\n"
 	CreateCSVFile(fname, data)
-	return
 
 	allRootOptions.Amount = 1
 	allRootOptions.Unit = "days"
 	allRootOptions.Column = 1
 
 	missingDates, csvStyleDate := SearchOneFile(fname)
+
 	iss := is.New(t)
 	iss.Equal(len(missingDates), 4)
-	iss.Equal(missingDates[0].ToTime().String(), "2021-04-15 03:00:00 +0000 UTC")
-	iss.Equal(missingDates[1].ToTime().String(), "2021-04-16 04:00:00 +0000 UTC")
-	iss.Equal(missingDates[2].ToTime().String(), "2021-04-17 05:00:00 +0000 UTC")
-	iss.Equal(missingDates[3].ToTime().String(), "2021-04-18 06:00:00 +0000 UTC")
+	iss.Equal(missingDates[0].ToTime().String(), "2021-04-15 00:00:00 +0000 UTC")
+	iss.Equal(missingDates[1].ToTime().String(), "2021-04-16 00:00:00 +0000 UTC")
+	iss.Equal(missingDates[2].ToTime().String(), "2021-04-17 00:00:00 +0000 UTC")
+	iss.Equal(missingDates[3].ToTime().String(), "2021-04-18 00:00:00 +0000 UTC")
 	iss.Equal(csvStyleDate, "2021-04-12")
 }
 
@@ -83,30 +83,30 @@ func TestSearch3(t *testing.T) {
 	fname := "TestSearch3.csv"
 	data := "Date,Total\n2021-03-10 18:40:01,317\n2021-03-11 18:40:01,249\n2021-03-15 18:40:04,287\n2021-03-16 18:40:03,320\n2021-03-19 18:40:06,102\n"
 	CreateCSVFile(fname, data)
-	return
 
-	allRootOptions.Amount = 25
+	allRootOptions.Amount = 24
 	allRootOptions.Unit = "hours"
+	allRootOptions.Padding = "1m"
 	allRootOptions.Column = 0
 
 	missingDates, csvStyleDate := SearchOneFile(fname)
-	//debug(missingDates, csvStyleDate)
+
 
 	iss := is.New(t)
 	iss.Equal(len(missingDates), 5)
-	iss.Equal(missingDates[0].ToTime().String(), "2021-03-12 20:40:01 +0000 UTC")
-	iss.Equal(missingDates[1].ToTime().String(), "2021-03-13 21:40:01 +0000 UTC")
-	iss.Equal(missingDates[2].ToTime().String(), "2021-03-14 22:40:01 +0000 UTC")
-	iss.Equal(missingDates[3].ToTime().String(), "2021-03-18 01:40:01 +0000 UTC")
-	iss.Equal(missingDates[4].ToTime().String(), "2021-03-19 02:40:01 +0000 UTC")
+	iss.Equal(missingDates[0].ToTime().String(), "2021-03-12 18:40:01 +0000 UTC")
+	iss.Equal(missingDates[1].ToTime().String(), "2021-03-13 18:40:01 +0000 UTC")
+	iss.Equal(missingDates[2].ToTime().String(), "2021-03-14 18:40:01 +0000 UTC")
+	iss.Equal(missingDates[3].ToTime().String(), "2021-03-17 18:40:01 +0000 UTC")
+	iss.Equal(missingDates[4].ToTime().String(), "2021-03-18 18:40:01 +0000 UTC")
 	iss.Equal(csvStyleDate, "2021-03-10 18:40:01")
 
 	allRootOptions.SkipWeekends = true
 	missingDates, _ = SearchOneFile(fname)
 	iss.Equal(len(missingDates), 3)
-	iss.Equal(missingDates[0].ToTime().String(), "2021-03-12 20:40:01 +0000 UTC")
-	iss.Equal(missingDates[1].ToTime().String(), "2021-03-18 01:40:01 +0000 UTC")
-	iss.Equal(missingDates[2].ToTime().String(), "2021-03-19 02:40:01 +0000 UTC")
+	iss.Equal(missingDates[0].ToTime().String(), "2021-03-12 18:40:01 +0000 UTC")
+	iss.Equal(missingDates[1].ToTime().String(), "2021-03-17 18:40:01 +0000 UTC")
+	iss.Equal(missingDates[2].ToTime().String(), "2021-03-18 18:40:01 +0000 UTC")
 }
 
 // TestSearch4 - 3 missing dates, tab-delimited file
@@ -114,21 +114,20 @@ func TestSearch4(t *testing.T) {
 	fname := "TestSearch4.csv"
 	data := "Date\tCount\n2021-04-01 18:40:00\t318\n2021-04-02 18:40:00\t252\n2021-04-06 18:40:00\t291\n2021-04-07 18:40:00\t274\n2021-04-08 18:40:01\t243"
 	CreateCSVFile(fname, data)
-	return
 
-	allRootOptions.Amount = 25
+	allRootOptions.Amount = 24
 	allRootOptions.Unit = "hours"
+	allRootOptions.Padding = "1m"
 	allRootOptions.Column = 0
 	allRootOptions.CsvDelimiter = "\t"
 	allRootOptions.SkipWeekends = false
 
 	missingDates, csvStyleDate := SearchOneFile(fname)
-
 	iss := is.New(t)
 	iss.Equal(len(missingDates), 3)
-	iss.Equal(missingDates[0].ToTime().String(), "2021-04-03 20:40:00 +0000 UTC")
-	iss.Equal(missingDates[1].ToTime().String(), "2021-04-04 21:40:00 +0000 UTC")
-	iss.Equal(missingDates[2].ToTime().String(), "2021-04-05 22:40:00 +0000 UTC")
+	iss.Equal(missingDates[0].ToTime().String(), "2021-04-03 18:40:00 +0000 UTC")
+	iss.Equal(missingDates[1].ToTime().String(), "2021-04-04 18:40:00 +0000 UTC")
+	iss.Equal(missingDates[2].ToTime().String(), "2021-04-05 18:40:00 +0000 UTC")
 	iss.Equal(csvStyleDate, "2021-04-01 18:40:00")
 }
 
@@ -137,20 +136,19 @@ func TestSearch5(t *testing.T) {
 	fname := "TestSearch5.csv"
 	data := "2021-04-01 18:40:00,318\n2021-04-02 18:40:00,252\n2021-04-06 18:40:00,291\n2021-04-07 18:40:00,274\n2021-04-08 18:40:01,243"
 	CreateCSVFile(fname, data)
-	return
 
-	allRootOptions.Amount = 25
+	allRootOptions.Amount = 24
 	allRootOptions.Unit = "hours"
+	allRootOptions.Padding = "5s"
 	allRootOptions.Column = 0
 	allRootOptions.CsvDelimiter = ","
 	allRootOptions.SkipWeekends = true
 	allRootOptions.HasNoHeader = true
 
 	missingDates, csvStyleDate := SearchOneFile(fname)
-
 	iss := is.New(t)
 	iss.Equal(len(missingDates), 1)
-	iss.Equal(missingDates[0].ToTime().String(), "2021-04-05 22:40:00 +0000 UTC")
+	iss.Equal(missingDates[0].ToTime().String(), "2021-04-05 18:40:00 +0000 UTC")
 	iss.Equal(csvStyleDate, "2021-04-01 18:40:00")
 }
 
@@ -160,10 +158,10 @@ func TestSearch6(t *testing.T) {
 	// this has embedded tab characters
 	data := "Date	Errors	Warnings\n2021-04-13 06:55:01	0	23\n2021-04-16 06:55:01	0	23\n2021-04-17 06:55:01	0	19\n2021-04-18 06:55:01	0	18\n2021-04-19 06:55:01	0	23\n2021-04-22 06:55:01	0	17\n"
 	CreateCSVFile(fname, data)
-	return
 
 	allRootOptions.Amount = 86400
 	allRootOptions.Unit = "seconds"
+	allRootOptions.Padding = "5s"
 	allRootOptions.Column = 0
 	allRootOptions.CsvDelimiter = "\t"
 	allRootOptions.SkipWeekends = false
@@ -182,7 +180,6 @@ func TestSearch7(t *testing.T) {
 	fname := "TestSearch7.csv"
 	data := "2021-04-15 06:55:01,0,23\n2021-04-15 08:30:26,0,23\n2021-04-16 06:55:01,0,23\n2021-04-19 06:55:01,0,23\n"
 	CreateCSVFile(fname, data)
-	return
 
 	allRootOptions.Amount = 1
 	allRootOptions.Unit = "days"
